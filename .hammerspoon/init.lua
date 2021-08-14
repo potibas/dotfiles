ef13b5efa83457fc0cbf3a5fb04020a5b8722f72
Potiguar Faga Catalan <potiguar@potiguar.net>
2021-08-14 15:10:24 -0300

    hammerspoon: Fix show/hide Alacritty window

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,6 +1,6 @@
 hs.hotkey.bind({"command"}, ";", function()
-  app = hs.application.find('Alacritty')
-  if app and app:isFrontmost() then
+  app = hs.application.get('Alacritty')
+  if app ~= nil and app:isFrontmost() then
     app:hide()
   else
     hs.application.launchOrFocus("/Applications/Alacritty.app")
