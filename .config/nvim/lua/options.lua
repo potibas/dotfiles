7834e03df7d770b65accece33789ea3d25e7533d
Potiguar Catalan <potiguar@potiguar.net>
2023-03-22 14:34:37 -0300

    nvim: basic sanity options

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -1,1 +1,15 @@
 vim.g.mapleader = ','
+
+vim.opt.termguicolors = true
+vim.opt.bg = 'dark'
+vim.opt.showcmd = true
+vim.opt.number = true
+vim.opt.cmdheight = 1
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.softtabstop = 2
+vim.opt.expandtab = true
+vim.opt.signcolumn = 'yes'
+vim.opt.completeopt = 'menu,menuone,noselect'
+vim.opt.showmatch = true
+vim.opt.matchtime = 1
