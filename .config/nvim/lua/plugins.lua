1ce179e6116be2199095c5236a50587cd420faab
Potiguar Catalan <potiguar@potiguar.net>
2023-03-22 15:35:15 -0300

    nvim: tokionight

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,2 +1,17 @@
 return {
+
+  -- Colorscheme
+  {
+    'folke/tokyonight.nvim',
+    lazy = false,
+    config = function()
+      require('tokyonight').setup({
+        style = 'moon',
+        transparent = true,
+        dim_inactive = true,
+      })
+      vim.cmd([[colorscheme tokyonight-moon]])
+    end,
+  },
+
 }
