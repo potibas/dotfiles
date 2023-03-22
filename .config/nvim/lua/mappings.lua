25c44a88add0ad273c014474489e780403b460b9
Potiguar Catalan <potiguar@potiguar.net>
2023-03-22 15:52:28 -0300

    nvim: map j/k to gj/gk

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -3,3 +3,7 @@ vim.keymap.set('c', '%%', "<C-R>=expand('%:h').'/'<cr>")
 
 -- Switch to alternate file
 vim.keymap.set('n', '<leader><leader>', '<c-^>')
+
+-- Make j/k movement more natural when text is wrapped
+vim.keymap.set('n', 'j', 'gj')
+vim.keymap.set('n', 'k', 'gk')
