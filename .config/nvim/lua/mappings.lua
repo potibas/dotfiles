51a6217c72a6734f801ed3b8a02563ba98c99b48
Potiguar Catalan <potiguar@potiguar.net>
2023-03-22 16:19:54 -0300

    nvim: search history with c-p/c-n

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -7,3 +7,16 @@ vim.keymap.set('n', '<leader><leader>', '<c-^>')
 -- Make j/k movement more natural when text is wrapped
 vim.keymap.set('n', 'j', 'gj')
 vim.keymap.set('n', 'k', 'gk')
+
+-- Search history with C-P/C-N
+local wildmenumap = function (mapping, alternate)
+  return function ()
+    if vim.fn['wildmenumode']() == 1 then
+      return mapping
+    else
+      return alternate
+    end
+  end
+end
+vim.keymap.set('c', '<c-p>', wildmenumap('<c-p>', '<up>'), { expr = true })
+vim.keymap.set('c', '<c-n>', wildmenumap('<c-n>', '<down>'), { expr = true })
