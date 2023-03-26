869276e793aa01d9ac5aec122b306bdc8f99811f
Potiguar Catalan <potiguar@potiguar.net>
2023-03-25 21:17:13 -0300

    nvim: bootstrap vim-test

---
+++ .config/nvim/lua/plugins/vim-test.lua
@@ -0,0 +1,16 @@
+return {
+  'vim-test/vim-test',
+  dependencies = {
+    'preservim/vimux',
+  },
+  keys = {
+    { '<c-t><c-y>', ':w | :TestNearest<CR>', desc = 'Test Nearest' },
+    { '<c-t><c-t>', ':w | :TestLast<CR>', desc = 'Test Last' },
+    { '<c-t><c-f>', ':w | :TestFile<CR>', desc = 'Test File' },
+    { '<c-t><c-a>', ':w | :TestSuite<CR>', desc = 'Test Suite' },
+    { '<c-t><c-v>', ':TestVisit<CR>', desc = 'Test Visit' },
+  },
+  config = function()
+    vim.g['test#strategy'] = 'vimux'
+  end
+}
