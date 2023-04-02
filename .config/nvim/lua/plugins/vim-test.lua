0bbbe54d8bd739843fa14a5f7c0cf80811605ad6
Potiguar Catalan <potiguar@potiguar.net>
2023-04-02 11:05:09 -0300

    nvim: tryout new vim-test shorctuts

--- .config/nvim/lua/plugins/vim-test.lua
+++ .config/nvim/lua/plugins/vim-test.lua
@@ -5,10 +5,13 @@ return {
   },
   keys = {
     { '<leader>t', ':w | :TestNearest<cr>', desc = 'Test nearest' },
+    { '<leader>Tf', ':w | :TestFile<cr>', desc = 'Test file' },
+    { '<leader>Tl', ':w | :TestLast<cr>', desc = 'Test last' },
+    { '<leader>Ta', ':w | :TestSuite<cr>', desc = 'Test suite' },
+    { '<leader>Tv', ':w | :TestVisit<cr>', desc = 'Test visit' },
   },
   config = function()
     vim.g['test#strategy'] = 'vimux'
     vim.g['test#preserve_screen'] = 0
-    vim.g['test#no_alternate'] = 1
   end
 }
