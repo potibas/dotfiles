70a8c431eedae3db908f2903229cba1c28ed0a5d
Potiguar Catalan <potiguar@potiguar.net>
2023-04-03 09:44:02 -0300

    nvim: indenting and search options

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -7,14 +7,24 @@ vim.opt.showcmd = true
 vim.opt.number = true
 vim.opt.cursorline = true
 vim.opt.cmdheight = 1
-vim.opt.tabstop = 2
-vim.opt.shiftwidth = 2
-vim.opt.softtabstop = 2
-vim.opt.expandtab = true
 vim.opt.signcolumn = 'yes'
 vim.opt.completeopt = 'menu,menuone,noselect'
 vim.opt.showmatch = true
 vim.opt.matchtime = 1
+
+-- Indenting
+vim.opt.shiftwidth = 2
+vim.opt.softtabstop = 2
+vim.opt.tabstop = 2
+vim.opt.expandtab = true
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+
+-- Searching
+vim.opt.incsearch = true
+vim.opt.hlsearch = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
 
 -- Don't insert comments on new lines
 vim.cmd [[autocmd FileType * setlocal formatoptions-=c formatoptions-=r formatoptions-=o]]
