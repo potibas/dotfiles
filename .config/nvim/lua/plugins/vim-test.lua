743fd20a64a95c10a3e60fdc17824521911d0131
Potiguar Catalan <potiguar@potiguar.net>
2023-04-03 09:47:26 -0300

    nvim: new tentative test shortcuts

--- .config/nvim/lua/plugins/vim-test.lua
+++ .config/nvim/lua/plugins/vim-test.lua
@@ -4,14 +4,15 @@ return {
     'preservim/vimux',
   },
   keys = {
-    { '<leader>t', ':w | :TestNearest<cr>', desc = 'Test nearest' },
-    { '<leader>Tf', ':w | :TestFile<cr>', desc = 'Test file' },
-    { '<leader>Tl', ':w | :TestLast<cr>', desc = 'Test last' },
-    { '<leader>Ta', ':w | :TestSuite<cr>', desc = 'Test suite' },
-    { '<leader>Tv', ':w | :TestVisit<cr>', desc = 'Test visit' },
+    { '<leader>t', ':w | :TestLast<cr>', desc = 'Test last' },
+    { '<space>tt', ':w | :TestNearest<cr>', desc = 'Test nearest' },
+    { '<space>tf', ':w | :TestFile<cr>', desc = 'Test file' },
+    { '<space>ta', ':w | :TestSuite<cr>', desc = 'Test suite' },
+    { '<space>tv', ':w | :TestVisit<cr>', desc = 'Test visit' },
   },
   config = function()
     vim.g['test#strategy'] = 'vimux'
     vim.g['test#preserve_screen'] = 0
+    vim.g['test#no_alternate'] = 1
   end
 }
