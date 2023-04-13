f098f2be163a77c329772c65c9ba4cc4c96aee20
Potiguar Catalan <potiguar@potiguar.net>
2023-04-12 21:03:21 -0300

    nvim: add search highlights

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -11,6 +11,8 @@ return {
         dim_inactive = true,
       })
       vim.cmd([[colorscheme tokyonight-moon]])
+      vim.cmd([[highlight IncSearch cterm=underline,italic guibg='NONE' guifg=#ffff00 gui=underline,italic]])
+      vim.cmd([[highlight Search cterm=underline,italic guibg='NONE' guifg=#ffff00 gui=underline,italic]])
     end,
   },
 
