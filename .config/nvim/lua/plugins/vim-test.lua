fc8d67b42b997109c30562ca61f6e8146509de60
Potiguar Catalan <potiguar@potiguar.net>
2023-04-14 19:36:20 -0300

    nvim: the need for test speed

--- .config/nvim/lua/plugins/vim-test.lua
+++ .config/nvim/lua/plugins/vim-test.lua
@@ -5,11 +5,12 @@ return {
   },
   keys = {
     { '<leader>tn', ':w | :TestNearest<cr>', silent = true, desc = 'Test nearest' },
-    { '<leader>tl', ':w | :TestFile<cr>',    silent = true, desc = 'Test last' },
+    { '<leader>tl', ':w | :TestLast<cr>',    silent = true, desc = 'Test last' },
+    { '\\',         ':w | :TestLast<cr>',    silent = true, desc = 'Test last' },
+    { ',',          ':w | :TestLast<cr>',    silent = true, desc = 'Test last' },
     { '<leader>tf', ':w | :TestFile<cr>',    silent = true, desc = 'Test file' },
     { '<leader>ta', ':w | :TestSuite<cr>',   silent = true, desc = 'Test suite' },
     { '<leader>tv', ':w | :TestVisit<cr>',   silent = true, desc = 'Test visit' },
-    { ',t',         ':w | :TestLast<cr>',    silent = true, desc = 'Test last' },
   },
   config = function()
     vim.g['test#strategy'] = 'vimux'
