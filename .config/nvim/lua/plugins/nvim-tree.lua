d3e680877589f8ea07dca7d1afc283375dfdeabe
Potiguar Catalan <potiguar@potiguar.net>
2023-04-14 09:51:41 -0300

    nvim: bootstrap nvim-tree

---
+++ .config/nvim/lua/plugins/nvim-tree.lua
@@ -0,0 +1,30 @@
+return {
+  'kyazdani42/nvim-tree.lua',
+  dependencies = {
+    'kyazdani42/nvim-web-devicons',
+  },
+  keys = {
+    { '<c-b>', ':NvimTreeFindFileToggle<CR>', desc = "Open File Tree" },
+  },
+  config = function()
+    require('nvim-tree').setup {
+      diagnostics = {
+        enable = true,
+      },
+      view = {
+        width = 45,
+        side = 'left'
+      },
+      actions = {
+        change_dir = {
+          enable = false,
+          global = false,
+        },
+      }
+    }
+
+    vim.cmd [[hi! NvimTreeNormalNC guibg=none ctermbg=none ]]
+    vim.cmd [[hi! NvimTreeNormal guibg=none ctermbg=none ]]
+    vim.cmd [[hi! NvimTreeWinSeparator guibg=none ctermbg=none ]]
+  end
+}
