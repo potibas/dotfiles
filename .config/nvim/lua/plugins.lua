bb57935fefcdd9d0e65aba997d8140dc53200e80
Potiguar Catalan <potiguar@potiguar.net>
2023-04-19 12:50:31 -0300

    nvim: material colorscheme

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,21 +1,5 @@
 return {
 
-  -- Colorscheme
-  {
-    'folke/tokyonight.nvim',
-    lazy = false,
-    config = function()
-      require('tokyonight').setup({
-        style = 'moon',
-        transparent = true,
-        dim_inactive = true,
-      })
-      vim.cmd([[colorscheme tokyonight-moon]])
-      vim.cmd([[highlight IncSearch cterm=underline,italic guibg='NONE' guifg=#ffff00 gui=underline,italic]])
-      vim.cmd([[highlight Search cterm=underline,italic guibg='NONE' guifg=#ffff00 gui=underline,italic]])
-    end,
-  },
-
   -- Essentials
   'tpope/vim-commentary',
   'tpope/vim-surround',
