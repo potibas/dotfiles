21b085571f32b88041af7a735fc626cbf2ab70a2
Potiguar Catalan <potiguar@potiguar.net>
2023-04-21 19:55:13 -0300

    Alt key for tmux-related shortcuts

--- .config/nvim/lua/plugins/navigator.lua
+++ .config/nvim/lua/plugins/navigator.lua
@@ -2,10 +2,9 @@ return {
   'numToStr/Navigator.nvim',
   config = true,
   keys = {
-    { '<c-h>', '<cmd>NavigatorLeft<cr>', desc = 'Navigate Left' },
-    { '<c-l>', '<cmd>NavigatorRight<cr>', desc = 'Navigate Right' },
-    { '<c-k>', '<cmd>NavigatorUp<cr>', desc = 'Navigate Up' },
-    { '<c-j>', '<cmd>NavigatorDown<cr>', desc = 'Navigate Down' },
-    { '<c-\\>', '<cmd>NavigatorPrevious<cr>', desc = 'Navigate Previous' },
+    { '˙',  '<cmd>NavigatorLeft<cr>',  desc = 'Navigate Left' },
+    { '¬',  '<cmd>NavigatorRight<cr>', desc = 'Navigate Right' },
+    { '˚',  '<cmd>NavigatorUp<cr>',    desc = 'Navigate Up' },
+    { '∆', '<cmd>NavigatorDown<cr>',  desc = 'Navigate Down' },
   }
 }
