43dfb551fe5ac2249eb1a91ec444689755b58d11
Potiguar Catalan <potiguar@potiguar.net>
2024-04-17 13:30:49 -0300

    Add more global shortcuts to hammerspoon

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,8 +1,29 @@
-hs.hotkey.bind({ 'command' }, ';', function()
-  app = hs.application.find('Kitty')
+local function showApplication(name)
+  local app = hs.application.find(name)
+  if not (app == nil) and not app:isFrontmost() then
+    app = hs.application.launchOrFocus('/Applications/' .. name .. '.app')
+    app.show()
+  end
+end
+
+local function toggleApplication(name)
+  local app = hs.application.find(name)
   if not (app == nil) and app:isFrontmost() then
     app:hide()
   else
-    hs.application.launchOrFocus('/Applications/Kitty.app')
+    app = hs.application.launchOrFocus('/Applications/' .. name .. '.app')
+    app.show()
   end
+end
+
+hs.hotkey.bind({ 'command' }, ';', function()
+  toggleApplication('Kitty')
+end)
+
+hs.hotkey.bind({ 'command' }, "/", function()
+  showApplication('Google Chrome')
+end)
+
+hs.hotkey.bind({ 'command' }, "'", function()
+  showApplication('WhatsApp')
 end)
