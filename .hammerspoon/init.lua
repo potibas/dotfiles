653daa4813e19f86c54caded3fad73208262e247
Potiguar Catalan <potiguar@potiguar.net>
2024-04-26 18:22:17 -0300

    Simplify hammerspoon config

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,29 +1,16 @@
-local function showApplication(name)
-  local app = hs.application.find(name)
-  if not (app == nil) and not app:isFrontmost() then
-    app = hs.application.launchOrFocus('/Applications/' .. name .. '.app')
-    app.show()
-  end
-end
-
-local function toggleApplication(name)
-  local app = hs.application.find(name)
-  if not (app == nil) and app:isFrontmost() then
-    app:hide()
-  else
-    app = hs.application.launchOrFocus('/Applications/' .. name .. '.app')
-    app.show()
-  end
-end
+-- install `hs` cli tool
+hs.ipc.cliInstall()
+hs.ipc.cliSaveHistory(true)
+hs.ipc.cliSaveHistorySize(999999)
 
 hs.hotkey.bind({ 'command' }, ';', function()
-  toggleApplication('Kitty')
+  hs.application.launchOrFocus('/Applications/kitty.app')
 end)
 
 hs.hotkey.bind({ 'command' }, "/", function()
-  showApplication('Google Chrome')
+  hs.application.launchOrFocus('/Applications/Google Chrome.app')
 end)
 
 hs.hotkey.bind({ 'command' }, "'", function()
-  showApplication('WhatsApp')
+  hs.application.launchOrFocus('/Applications/WhatsApp.app')
 end)
