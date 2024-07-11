cb0fa39f7259d82de709dae0132d48ba5bf84655
Potiguar Catalan <potiguar@potiguar.net>
2024-07-10 23:05:03 -0300

    nvim: configure supermaven

--- .config/nvim/lua/plugins/supermaven.lua
+++ .config/nvim/lua/plugins/supermaven.lua
@@ -1,10 +1,22 @@
 return {
   'supermaven-inc/supermaven-nvim',
   config = function()
-    require('supermaven-nvim').setup({
-      log_level = 'off',
-      disable_inline_completion = true,
-      disable_keymaps = true,
+    require("supermaven-nvim").setup({
+      log_level = "info",                  -- set to "off" to disable logging completejbjy
+      disable_inline_completion = false,   -- set to true if you want to use cmp for that
+      disable_keymaps = false,             -- disables built in keymaps for more manual control
+      keymaps = {
+        accept_suggestion = "<tab>",
+        clear_suggestion = "<c-c>",
+        accept_word = "<c-j>",
+      },
+      ignore_filetypes = {
+        cpp = true,
+      }
+      -- color = {
+      --   suggestion_color = "#ffffff",
+      --   cterm = 244,
+      -- },
     })
   end,
 }
