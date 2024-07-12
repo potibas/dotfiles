a968d11818b2cffe22bd01bdcf609718957c0efb
Potiguar Catalan <potiguar@potiguar.net>
2024-07-12 19:10:51 -0300

    nvim: higlight searches with yellow

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -2,11 +2,25 @@ return {
   'olimorris/onedarkpro.nvim',
   lazy = false,
   config = function()
-    api = require('onedarkpro')
+    local api = require('onedarkpro')
+
+    --[====[
+   local helpers = require('onedarkpro.helpers')
+   local colors = helpers.get_preloaded_colors()
+
+   print(colors)
+   --]====]
+
     api.setup({
       highlights = {
         -- Bring attention to errors in the git commit message
         ['@gitcommit_error.gitcommit'] = { reverse = true },
+
+        -- Search
+        Search = { fg = 'yellow', bold = true, italic = true, underline = true },
+        CurSearch = { fg = 'white', bold = true, italic = true, underline = true, reverse = true },
+        IncSearch = { link = 'Search' },
+
       },
     })
 
