2f2603f35d25ee742b6bf66c47044b70da5549dd
Potiguar Catalan <potiguar@potiguar.net>
2024-07-12 17:11:49 -0300

    nvim: add git commit msg ts query for > 50 chars

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -2,6 +2,14 @@ return {
   'olimorris/onedarkpro.nvim',
   lazy = false,
   config = function()
+    api = require('onedarkpro')
+    api.setup({
+      highlights = {
+        -- Bring attention to errors in the git commit message
+        ['@gitcommit_error.gitcommit'] = { reverse = true },
+      },
+    })
+
     vim.cmd([[colorscheme onedark_vivid]])
   end
 }
