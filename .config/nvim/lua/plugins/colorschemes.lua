96fe7f8f597d986c4d78d8cdd447958047fda8b4
Potiguar Catalan <potiguar@potiguar.net>
2024-07-13 03:36:51 -0300

    nvim: customize search highlights

--- .config/nvim/lua/plugins/colorschemes.lua
+++ .config/nvim/lua/plugins/colorschemes.lua
@@ -2,6 +2,19 @@ return {
   'olimorris/onedarkpro.nvim',
   lazy = false,
   config = function()
+
+    local api = require('onedarkpro')
+
+    api.setup({
+      highlights = {
+        -- Search
+        Search = { fg = 'yellow', bold = false, italic = true, underline = true },
+        CurSearch = { fg = 'white', bold = true, italic = true, underline = true },
+        IncSearch = { link = 'Search' },
+      },
+    })
+
     vim.cmd([[colorscheme onedark_vivid]])
+
   end
 }
