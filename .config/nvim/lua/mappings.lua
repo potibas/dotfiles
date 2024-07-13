f73b337d125deae0a6b71b504b8073a7ffc21b6b
Potiguar Catalan <potiguar@potiguar.net>
2024-07-13 03:06:11 -0300

    nvim: map `H` and `L` to act as home/end keys

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -4,3 +4,7 @@ local map = vim.keymap.set
 map('n', '<leader>w', ':w<cr>', { silent = true, desc = 'Write buffer' })
 map('n', '<leader>q', ':q<cr>', { silent = true, desc = 'Quit the current window' })
 map('n', '<leader>e', ':e!<cr>', { silent = true, desc = 'Discard unsaved changes in current buffer' })
+
+-- Home/End with H/L
+map('n', 'H', '_', { silent = true, desc = 'Move to the start of the line' })
+map('n', 'L', '$', { silent = true, desc = 'Move to the end of the line' })
