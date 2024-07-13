db85a9410275e6c7418eee11e3b7b5c63f8ead1f
Potiguar Catalan <potiguar@potiguar.net>
2024-07-13 03:03:27 -0300

    nvim: map `<leader>e` to `:e!`

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -3,3 +3,4 @@ local map = vim.keymap.set
 -- Basic operations
 map('n', '<leader>w', ':w<cr>', { silent = true, desc = 'Write buffer' })
 map('n', '<leader>q', ':q<cr>', { silent = true, desc = 'Quit the current window' })
+map('n', '<leader>e', ':e!<cr>', { silent = true, desc = 'Discard unsaved changes in current buffer' })
