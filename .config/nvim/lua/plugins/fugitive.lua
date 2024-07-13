b7c70ca00adc5f2a784d9d5f2609e4b3b3929f96
Potiguar Catalan <potiguar@potiguar.net>
2024-07-12 21:53:26 -0300

    fix: tabula rasa

--- .config/nvim/lua/plugins/fugitive.lua
+++
@@ -1,8 +0,0 @@
-return {
-  'tpope/vim-fugitive',
-  lazy = false,
-  keys = {
-    { '<c-g>s', ':vertical Git<cr>',      silent = true, desc = 'Opens the git status window' },
-    { '<c-g>d', ':Git diff --cached<cr>', silent = true, desc = 'Opens the diff for the staging area' },
-  }
-}
