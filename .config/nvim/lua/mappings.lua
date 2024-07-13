d96e82fe36cb569a00c599ac972f477c7e04636e
Potiguar Catalan <potiguar@potiguar.net>
2024-07-13 03:02:38 -0300

    nvim: map `<leader>q` to `:q`

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -2,3 +2,4 @@ local map = vim.keymap.set
 
 -- Basic operations
 map('n', '<leader>w', ':w<cr>', { silent = true, desc = 'Write buffer' })
+map('n', '<leader>q', ':q<cr>', { silent = true, desc = 'Quit the current window' })
