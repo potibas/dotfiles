c71aafa279d6b2ff67e7fa31eca27211dc87b11e
Potiguar Catalan <potiguar@potiguar.net>
2024-07-13 17:10:37 -0300

    wip catppuccino

--- .config/nvim/lua/plugins/colorschemes.lua
+++ .config/nvim/lua/plugins/colorschemes.lua
@@ -1,23 +1,41 @@
+local custom_highlights = function(colors)
+  return {
+    -- Bring attention to errors in the git commit message
+    ['@gitcommit_error.gitcommit'] = { reverse = true },
+
+    -- Search
+    Search = { italic = true, underline = true, fg = colors.yellow, bg = 'NONE'  },
+    CurSearch = { italic = true, underline = true, fg = colors.blue, bold = true, bg = 'NONE' },
+    IncSearch = { link = 'Search' },
+  }
+end
+
 return {
-  'olimorris/onedarkpro.nvim',
-  lazy = false,
-  config = function()
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    lazy = false,
+    priority = 1000,
 
-    local api = require('onedarkpro')
+    config = function()
+      require('catppuccin').setup({
+        flavour = 'mocha',
+        custom_highlights = custom_highlights,
+      })
 
-    api.setup({
-      highlights = {
-        -- Bring attention to errors in the git commit message
-        ['@gitcommit_error.gitcommit'] = { reverse = true },
+      vim.cmd.colorscheme('catppuccin')
+    end,
+  },
+  {
+    'olimorris/onedarkpro.nvim',
+    lazy = false,
+    config = function()
+      local api = require('onedarkpro')
+      local colors = require('onedarkpro.helpers').get_preloaded_colors()
 
-        -- Search
-        Search = { fg = 'yellow', bold = false, italic = true, underline = true },
-        CurSearch = { fg = 'white', bold = true, italic = true, underline = true },
-        IncSearch = { link = 'Search' },
-      },
-    })
-
-    vim.cmd([[colorscheme onedark_vivid]])
-
-  end
+      api.setup({
+        highlights = custom_highlights(colors),
+      })
+    end
+  },
 }
