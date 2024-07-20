ff789da94a6e170d00790f7d4d0428dcee419c7e
Potiguar Faga Catalan <potiguar@potiguar.net>
2024-07-20 03:10:09 -0300

    nvim: map `<leader>rr` to reload neovim

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -1,3 +1,4 @@
 -- Basic buffer operations
 vim.keymap.set("n", "<leader>w", ":w<cr>", { silent = true, desc = "Write buffer" })
 vim.keymap.set("n", "<leader>e", ":e!<cr>", { silent = true, desc = "Reload current file" })
+vim.keymap.set("n", "<leader>rr", ":cq<cr>", { silent = true, desc = "Exit Neovim and opens it again" })
