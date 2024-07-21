c791a86fb988f55b1f4b6787235d8dc7b46b96ef
Potiguar Faga Catalan <potiguar@potiguar.net>
2024-07-21 20:15:08 -0300

    nvim: install vim-test

---
+++ .config/nvim/lua/plugins/vim-test.lua
@@ -0,0 +1,35 @@
+return {
+	"vim-test/vim-test",
+
+	dependencies = {
+		"preservim/vimux",
+	},
+
+	keys = {
+		{ "<leader>tn", ":w | :TestNearest<cr>", silent = true, desc = "Test nearest" },
+		{ "<leader>tl", ":w | :TestLast<cr>", silent = true, desc = "Test last" },
+		{ "\\", ":w | :TestNearest<cr>", silent = true, desc = "Test nearest" },
+		{ "<leader>.", ":w | :TestLast<cr>", silent = true, desc = "Test last" },
+		{ "<leader>tf", ":w | :TestFile<cr>", silent = true, desc = "Test file" },
+		{ "<leader>ta", ":w | :TestSuite<cr>", silent = true, desc = "Test suite" },
+		{ "<leader>tv", ":w | :TestVisit<cr>", silent = true, desc = "Test visit" },
+	},
+
+	config = function()
+		vim.g["test#strategy"] = "vimux"
+		vim.g["test#preserve_screen"] = 0
+		vim.g["test#no_alternate"] = 1
+
+		-- PHP laravel
+		vim.g["test#php#phpunit#executable"] = "php artisan test --order-by=random"
+
+		vim.cmd([[
+      function! RemoveExtraColorArg(cmd) abort
+        return substitute(a:cmd, " --colors", "", "")
+      endfunction
+
+      let g:test#custom_transformations = {'php#phpunit': function('RemoveExtraColorArg')}
+      let g:test#transformation = 'php#phpunit'
+    ]])
+	end,
+}
