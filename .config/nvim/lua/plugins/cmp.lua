42c3688497ce5f4f6961241d3570400d79b46e54
Potiguar Faga Catalan <potiguar@potiguar.net>
2024-07-23 02:29:42 -0300

    nvim: install cmp

---
+++ .config/nvim/lua/plugins/cmp.lua
@@ -0,0 +1,32 @@
+return {
+	"hrsh7th/nvim-cmp",
+
+	dependencies = {
+		"hrsh7th/cmp-nvim-lsp",
+		"hrsh7th/cmp-path",
+	},
+
+	config = function()
+		local cmp = require("cmp")
+
+		cmp.setup({
+			window = {
+				completion = cmp.config.window.bordered(),
+				documentation = cmp.config.window.bordered(),
+			},
+			mapping = {
+				["<c-c>"] = cmp.mapping.abort(),
+				["<c-d>"] = cmp.mapping.scroll_docs(-4),
+				["<c-u>"] = cmp.mapping.scroll_docs(4),
+				["<cr>"] = cmp.mapping.confirm({ select = true }),
+				["<c-n>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i" }),
+				["<c-p>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i" }),
+			},
+
+			sources = cmp.config.sources({
+				{ name = "nvim_lsp" },
+				{ name = "path" },
+			}),
+		})
+	end,
+}
